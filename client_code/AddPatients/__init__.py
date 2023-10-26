25b8fc81cc3a11997dc13536d551d698a12001b7
cac346063@gmail.com <cac346063@gmail.com>
2023-10-26 22:58:02 +0000

    Created DisplayAPatient and AddPatients; Edited settings and DoctorUI

--- client_code/AddPatients/__init__.py
+++ client_code/AddPatients/__init__.py
@@ -1,23 +1,13 @@
-from ._anvil_designer import DoctorUITemplate
+from ._anvil_designer import AddPatientsTemplate
 from anvil import *
 import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ..Variables import AppState
 
-class DoctorUI(DoctorUITemplate):
-  Namess = AppState.DoctorName
+class AddPatients(AddPatientsTemplate):
   def __init__(self, **properties):
-
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    
 
     # Any code you write here will run before the form opens.
-
-  def button_1_click(self, **event_args):
-    open_form('AddPatients')
-    pass
-
-
