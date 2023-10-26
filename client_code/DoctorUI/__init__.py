e0791149cd53303f8e7ad93613deed606b6d6303
cac346063@gmail.com <cac346063@gmail.com>
2023-10-26 22:05:20 +0000

    Edited DoctorUI

--- client_code/DoctorUI/__init__.py
+++ client_code/DoctorUI/__init__.py
@@ -7,12 +7,14 @@ from anvil.tables import app_tables
 from ..Variables import AppState
 
 class DoctorUI(DoctorUITemplate):
+  def get_index(user, pas):
+        return anvil.server.call('getDIndex', user, pas)
   
-  
-  index = anvil.server.call('getDIndex', AppState.Dtypedu, AppState.Dtypedp)
+  index = get_index() AppState.Dtypedu, AppState.Dtypedp)
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     print(self.index)
 
     # Any code you write here will run before the form opens.
+
