b7428f36eeef974cac465805c6c8e802b6e9a9be
cac346063@gmail.com <cac346063@gmail.com>
2023-11-02 00:08:49 +0000

    Edited PatientLogin

--- client_code/PatientLogin/__init__.py
+++ client_code/PatientLogin/__init__.py
@@ -45,10 +45,8 @@ class PatientLogin(PatientLoginTemplate):
         AppState.ii=AppState.Pindex
         AppState.qqs=anvil.server.call('getQuestion', AppState.ii)
         AppState.aas=anvil.server.call('getAnswer', AppState.ii)
-        #if current_datetime.replace(tzinfo=timezone.utc) >= row['LastNoti'].replace(tzinfo=timezone.utc) + datetime.timedelta(seconds=row['Schedule']):
-        if current_datetime.replace(tzinfo=timezone.utc) >= row['LastNoti'].replace(tzinfo=timezone.utc) + datetime.timedelta(seconds=row['Schedule']): 
+        if current_datetime.replace(tzinfo=timezone.utc) >= row['LastNoti'].replace(tzinfo=timezone.utc) + datetime.timedelta(seconds=row['Schedule']):
          AppState.b = True
-         row['Schedule'] = 100000
           #row['LastNoti'] = current_datetime
         #change
         AppState.yes='Take ' + AppState.currentMed + " every 1 Day Next medication is at " + (app_tables.patient.search()[index]['LastNoti'].replace(tzinfo=timezone.utc) + datetime.timedelta(seconds=86400)).strftime('%Y-%m-%d %H:%M:%S') 
